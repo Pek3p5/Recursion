7bd70f48f24e92f74913d0748c5ca660073126ff
Pek3p5 <97115175+Pek3p5@users.noreply.github.com>
2023-11-14 13:14:57 +0300

    Add files via upload

---
+++ 64.cs
@@ -0,0 +1,21 @@
+﻿int GetNumber(string message)
+{
+    System.Console.WriteLine($"Введите число {message}");
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
+
+int RecMetod(int N)
+        {
+            if (N != 1)
+            {
+                System.Console.WriteLine(N);
+                N--;
+                return RecMetod(N);
+            }
+            return N;
+        }
+
+int N = GetNumber("Введите число N");
+System.Console.WriteLine( RecMetod(N));
---
+++ 66.cs
@@ -0,0 +1,18 @@
+﻿int GetNumber(string message)
+{
+    System.Console.WriteLine($"Введите число {message}");
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
+int M = GetNumber("Введите число M");
+int N = GetNumber("Введите число N");
+int sum = 0;
+
+while (M<=N)
+{
+    sum = sum + M;
+    M++;
+}
+
+System.Console.WriteLine(sum);
